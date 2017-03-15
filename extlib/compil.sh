43ace00247fe70a8f37754a506635c756d2bcc14
RUE Francois <francois.rue@inria.fr>
2017-03-15 14:02:20 +0100

    retrait du compil

--- extlib/compil.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcc src/slurm_to_json.c src/slurm_get_all.c -I./include/ -ljansson -lslurm -fPIC -shared -o ./lib/libwlm.so
-#gcc src/*.c -I./include -I${INCLUDEPATH} -fPIC -shared -o ../lib/libwlm.so 
-
